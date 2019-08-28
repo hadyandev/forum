52992683a328dc93a9e18c81a1f85c2f3c8c5c62
Hadyan Ahmad <hadyandev@gmail.com>
2019-08-28 23:06:52 +0700

    Lesson 2. Test-Driving Threads

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,16 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadController@index');
+Route::get('/threads/{thread}', 'ThreadController@show');
+// Route::resource('threads', 'ThreadController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
