e99629d25b70884f49e7fe0e067b8207b5bd3c29
Hadyan Ahmad <hadyandev@gmail.com>
2019-09-12 17:09:43 +0700

    Lesson 16. Meta Details and Pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,44 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center mb-4">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    <a href="">{{ $thread->creator->name }}</a> posted:
-                    {{$thread->title}}
-                </div>
-                <div class="card-body">
-                    <div class="body">{{ $thread->body }}</div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach ($thread->replies as $reply)
-                @include('threads.reply')
-            @endforeach
-        </div>
-    </div>
-
-    @if (auth()->check())
-        <div class="row justify-content-center">
+    <div class="container">
+        <div class="row justify-content-start mb-4">
             <div class="col-md-8">
-                <form method="POST" action="{{ $thread->path() . '/replies'}}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="body">Body:</label>
-                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                <div class="card mb-5">
+                    <div class="card-header">
+                        <a href="">{{ $thread->creator->name }}</a> posted:
+                        {{$thread->title}}
                     </div>
-                    <button type="submit" class="btn btn-dark">Post</button>
-                </form>
+                    <div class="card-body">
+                        <div class="body">{{ $thread->body }}</div>
+                    </div>
+                </div>
+
+                @foreach ($replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+
+                {{ $replies->links() }}
+            
+
+                @if (auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies'}}" class="mt-3">
+                        @csrf
+                        <div class="form-group">
+                            <label for="body">Body:</label>
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-dark">Post</button>
+                    </form>
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                @endif
+            </div>
+            
+            <div class="col-md-4">
+                <div class="card mb-5">
+                    <div class="card-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }}
+                            by <a href="">{{ $thread->creator->name }}</a>,
+                            and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count ) }}.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endif
-</div>
+    </div>
 @endsection
